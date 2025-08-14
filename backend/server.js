80860c7237adb8134af83517f89e018401fb256e
OriBerger <oriberger12@gmail.com>
2025-08-14 14:12:34 +0300

    Helfy task manager app

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import cors from "cors";
 import express, { json } from "express";
 import errorHandler from "./middleware/errorHandler.js";
-import taskRoutes from "./routes/tasks.js";
+import tasksRoutes from "./routes/tasks.js";
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(json());
-app.use("/api/tasks", taskRoutes);
+app.use("/api/tasks", tasksRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
